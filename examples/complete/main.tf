6753e9b0c2354ec2125288016a5383c5cedb770f
Jillian <jillian.e.rowe@gmail.com>
2021-10-13 10:59:32 +0300

    commit for release

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,7 +1,26 @@
-module "example" {
-  source = "../.."
+module "nginx_ingress" {
+  count                   = var.enable_ssl == true ? 1 : 0
+  source                  = "dabble-of-devops-bioanalyze/eks-bitnami-nginx-ingress/aws"
+  version                 = "0.0.2"
+  letsencrypt_email       = var.letsencrypt_email
+  helm_release_values_dir = var.helm_release_values_dir
+  helm_release_name       = var.helm_release_name
+}
 
-  example = var.example
+data "kubernetes_service" "airflow_ingress" {
+  count = var.enable_ssl == true ? 1 : 0
+  depends_on = [
+    module.airflow_ingress
+  ]
+  metadata {
+    name = "${var.helm_release_name}-ingress-nginx-ingress-controller"
+  }
+}
 
-  context = module.this.context
+data "aws_elb" "airflow_ingress" {
+  count = var.enable_ssl == true ? 1 : 0
+  depends_on = [
+    data.kubernetes_service.airflow_ingress
+  ]
+  name = split("-", data.kubernetes_service.airflow_ingress[0].status.0.load_balancer.0.ingress.0.hostname)[0]
 }
